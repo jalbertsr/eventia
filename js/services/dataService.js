0960e9e25d827cd6e9cede089c017371f2f6f25d
Humberto Acea <humbertoacea@gmail.com>
2017-07-24 13:13:21 +0200

    Keyworld service little change

--- js/services/dataService.js
+++ js/services/dataService.js
@@ -2,7 +2,7 @@ angular.module('tickets-app')
     .factory('dataService', function ($http) {
       var apiKey = 'bYHVec5iZZLcVLX9LTAK3Ipovub0RRvD'
 
-      function getKeywordSearchPlaying (keyword) {
+      function getKeywordSearch (keyword) {
         var url = 'https://app.ticketmaster.com/discovery/v2/events.json?keyword=' + keyword + '&apikey=' + apiKey
         return $http.get(url)
       }
