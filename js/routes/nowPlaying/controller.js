8f8766e07782bf05c8f8c3dbbaf9d4447a1fa590
Humberto Acea <humbertoacea@gmail.com>
2017-07-24 13:10:33 +0200

    Keyword service

--- js/routes/nowPlaying/controller.js
+++
@@ -1,9 +0,0 @@
-angular.module('angularMovies')
-    .controller('nowPlayingController', function($scope, dataService) {
-    	
-        dataService.getNowPlaying()
-            .then( function (response) {
-                console.log(response)
-                $scope.films = response.data.results
-            })
-    })
