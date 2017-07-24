880d6c9d1d2763d665275b3eac33ac0bd3a25395
Joan Albert Segura <jaxalbert96@gmail.com>
2017-07-25 00:03:11 +0200

    completed daily events and search

--- js/routes/home/controller.js
+++ js/routes/home/controller.js
@@ -1,27 +1,26 @@
 angular.module('tickets-app')
     .controller('searchController', function ($scope, $rootScope, dataService) {
+      
       $scope.getKeyword = function () {
-        var keyword = $scope.event
+        var keyword = $scope.keyword
         dataService.getKeywordSearch(keyword)
-                .then(function (response) {
-                  console.log(response)
-                })
-            // $rootScope.$broadcast('keywordReady', { keyword: keyword })
+          .then(function (response) {
+            console.log(response)
+          })
       }
-
-        // $scope.$on('keywordReady', function(e, data) {
-        //     dataService.getKeywordSearch(data.keyword)
-        //         .then(function(response) {
-        //             console.log("SDA")
-        //         })
-        // })
     })
+
     .controller('dailyController', function ($scope, $rootScope, dataService) {
+
       $scope.getDaily = function () {
-        var keyword = $scope.event
-        dataService.getKeywordSearch(keyword)
+        //YYYY-MM-DDTHH:mm:ssZ ---> format date 
+        var date = (new Date()).toISOString().split('.')
+        date = date.shift() + 'Z'
+        dataService.getKeywordSearch(date)
                 .then(function (response) {
                   console.log(response)
+                  var completeEvents = response.data.events
+                  $scope.events = 
                 })
       }
     })
