e1b4b954505b52080ae4d6b29ec12ca4b3a027b3
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-07-25 14:06:11 +0200

    parsed data for search results

--- js/routes/search-results/controller.js
+++ js/routes/search-results/controller.js
@@ -4,10 +4,30 @@
     var keyword = $routeParams.keyword
     console.log(keyword)
 
+    var months = {
+    	'1': 'JAN', 
+    	'2': 'FEB', 
+    	'3': 'MAR',
+    	'4': 'APR',
+    	'5': 'MAY',
+    	'6': 'JUN',
+    	'7': 'JUL',
+    	'8': 'AUG',
+    	'9': 'SEPT',
+    	'10': 'OCT',
+    	'11': 'NOV',
+    	'12': 'DEC'
+    }
+
     dataService.getKeywordSearch(keyword)
      .then(function(response) {
          console.log(response)
          $scope.events = response.data
+         var localDate = response.data._embedded.events["0"].dates.start.localDate
+         var parseMonth = localDate.split('-')[1]
+         $scope.month = months[parseMonth]
+         console.log($scope.month)
+
     })
 
   })
