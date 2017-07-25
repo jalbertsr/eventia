2a4a11fff1a3f3ef801052c7e47fb6d96c8467ce
Humberto Acea <humbertoacea@gmail.com>
2017-07-25 09:17:54 +0200

    Working!!

--- js/routes/home/controller.js
+++ js/routes/home/controller.js
@@ -1,6 +1,5 @@
 angular.module('tickets-app')
     .controller('searchController', function ($scope, $rootScope, dataService) {
-      
       $scope.getKeyword = function () {
         var keyword = $scope.keyword
         dataService.getKeywordSearch(keyword)
@@ -11,36 +10,37 @@ angular.module('tickets-app')
     })
 
     .controller('dailyController', function ($scope, $rootScope, dataService) {
-
-      $scope.getDaily = function () {
-        //YYYY-MM-DDTHH:mm:ssZ ---> format date 
-        var date = (new Date()).toISOString().split('.')
-        date = date.shift() + 'Z'
-        dataService.getKeywordSearch(date)
+      // $scope.getDaily = function () {
+        // YYYY-MM-DDTHH:mm:ssZ ---> format date
+      var date = (new Date()).toISOString().split('.')
+      date = date.shift() + 'Z'
+      dataService.getDaily(date)
                 .then(function (response) {
                   console.log(response)
-                  var completeEvents = response.data.events
-                  $scope.events = 
+                  var completeEvents = response.data._embedded.events
+                  console.log(completeEvents)
+                  $scope.events = completeEvents.slice(0, 4)
                 })
-      }
+      // }
     })
 
-    .controller('popularController', function ($scope, $rootScope, dataService) {
-      $scope.getPopular = function () {
-        var keyword = $scope.event
-        dataService.getKeywordSearch(keyword)
+    .controller('dailyController', function ($scope, $rootScope, dataService) {
+      dataService.getPopular()
                 .then(function (response) {
                   console.log(response)
+                  var completeEvents = response.data._embedded.events
+                  console.log(completeEvents)
+                  $scope.events = completeEvents.slice(0, 4)
                 })
-      }
+      // }
     })
 
-    .controller('relevantController', function ($scope, $rootScope, dataService) {
-      $scope.getRelevant = function () {
-        var keyword = $scope.event
-        dataService.getKeywordSearch(keyword)
-                .then(function (response) {
-                  console.log(response)
-                })
-      }
-    })
+    // .controller('relevantController', function ($scope, $rootScope, dataService) {
+    //  // $scope.getRelevant = function () {
+    //   var keyword = $scope.event
+    //   dataService.getKeywordSearch(keyword)
+    //             .then(function (response) {
+    //               console.log(response)
+    //             })
+    //  // }
+    // })
--- js/routes/home/index.js
+++ js/routes/home/index.js
@@ -1,8 +1,8 @@
 angular.module('tickets-app')
-.config(function($routeProvider){
-	$routeProvider
-	.when('/',{
-		templateUrl : 'js/routes/home/template.html',
-		controller : 'searchController', 'dailyController', 'popularController', 'relevantController'
-	})
+.config(function ($routeProvider) {
+  $routeProvider
+	.when('/', {
+  templateUrl: 'js/routes/home/template.html'
+		// controller : 'searchController', 'dailyController', 'popularController', 'relevantController'
+})
 })
