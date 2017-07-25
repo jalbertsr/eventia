1512eee8bc1e581a43a72f01a25f2dfe1cddf314
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-07-25 18:43:10 +0200

    created incrutsed map

--- js/routes/detailsEvent/controller.js
+++ js/routes/detailsEvent/controller.js
@@ -5,7 +5,8 @@ angular.module('tickets-app')
     dataService.getEvent(idEvent)
         .then(function (response) {
           $scope.event = response.data
-
+          $scope.latitude = response.data._embedded.venues["0"].location.latitude
+          $scope.longitude = response.data._embedded.venues["0"].location.longitude
           console.log(response)
         })
   })
